460769c0dd149e8d7c67551c44f3e994bf2fb4f0
manasapanda-oditek <manasapanda2020@gmail.com>
2024-05-20 12:32:18 +0530

    innitial stages

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { DataTablesModule } from 'angular-datatables';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DataTablesModule,
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+//import { DataTables } from 'angular-datatables'
+
+//import { DataTablesOptions } from 'angular-datatables'
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+
+  
+  dtOptions = {};
+
+ 
+}
